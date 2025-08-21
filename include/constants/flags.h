fecf6d7781bd90616c9f747a52c594469d54ce4a
Art <art@Claymaker.sn>
2025-08-21 01:09:36 -0400

    Bunch of config changes.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -47,8 +47,8 @@
 #define FLAG_UNUSED_0x021    0x21 // Dexnav
 #define FLAG_UNUSED_0x022    0x22 // Dexnav
 #define FLAG_UNUSED_0x023    0x23 // Dexnav
-#define FLAG_UNUSED_0x024    0x24 // Unused Flag
-#define FLAG_UNUSED_0x025    0x25 // Unused Flag
+#define FLAG_UNUSED_0x024    0x24 // Follower NPCs
+#define FLAG_UNUSED_0x025    0x25 // Follower NPCs
 #define FLAG_UNUSED_0x026    0x26 // Unused Flag
 #define FLAG_UNUSED_0x027    0x27 // Unused Flag
 #define FLAG_UNUSED_0x028    0x28 // Unused Flag
@@ -656,7 +656,7 @@
 #define FLAG_HIDDEN_ITEM_ROUTE_123_RARE_CANDY                (FLAG_HIDDEN_ITEMS_START + 0x6E)
 #define FLAG_HIDDEN_ITEM_ROUTE_105_BIG_PEARL                 (FLAG_HIDDEN_ITEMS_START + 0x6F)
 
-#define FLAG_UNUSED_0x264  0x264 // Unused Flag
+#define FLAG_UNUSED_0x264  0x264 // Exp. Share
 #define FLAG_UNUSED_0x265  0x265 // Unused Flag
 #define FLAG_UNUSED_0x266  0x266 // Unused Flag
 #define FLAG_UNUSED_0x267  0x267 // Unused Flag
